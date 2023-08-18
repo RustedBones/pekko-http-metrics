bf94a66020218565cb6173f3830674e02326c4c6
Scala Steward <scala_steward@virtuslab.com>
2023-08-18 03:52:48 +0000

    Update enumeratum to 1.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val Datadog               = "4.2.0"
     val Dropwizard            = "4.2.19"
     val DropwizardV5          = "5.0.0"
-    val Enumeratum            = "1.7.2"
+    val Enumeratum            = "1.7.3"
     val Logback               = "1.4.8"
     val Mockito               = "5.4.0"
     val Pekko                 = "1.0.1"

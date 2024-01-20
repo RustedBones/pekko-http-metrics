bb48f44ced3786b7be2eba4a135e15a9ef198526
Scala Steward <scala_steward@virtuslab.com>
2024-01-20 18:44:58 +0000

    Update metrics-core, metrics-json to 4.2.24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val Datadog               = "4.2.0"
-    val Dropwizard            = "4.2.23"
+    val Dropwizard            = "4.2.24"
     val DropwizardV5          = "5.0.0"
     val Enumeratum            = "1.7.3"
     val Logback               = "1.4.14"

64f499a09d31fdeb241a7570d5b20bcdc75cf2cc
Scala Steward <scala_steward@virtuslab.com>
2024-01-26 18:37:38 +0000

    Update java-dogstatsd-client to 4.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Datadog               = "4.2.0"
+    val Datadog               = "4.3.0"
     val Dropwizard            = "4.2.23"
     val DropwizardV5          = "5.0.0"
     val Enumeratum            = "1.7.3"

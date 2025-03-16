0fead8b6f674c9050c775916ea1a45fe90c08954
Michel Davit <michel@davit.fr>
2025-03-16 16:32:00 +0100

    Cleanup sampling configuration

--- prometheus/src/main/scala/fr/davit/pekko/http/metrics/prometheus/PrometheusSettings.scala
+++ prometheus/src/main/scala/fr/davit/pekko/http/metrics/prometheus/PrometheusSettings.scala
@@ -24,13 +24,16 @@ import fr.davit.pekko.http.metrics.prometheus.Quantiles.Quantile
 import scala.collection.immutable
 import scala.concurrent.duration._
 
-sealed trait HistogramConfig
-
-sealed trait TimerConfig
+/** SamplingConfig configures the metric sampling.
+  *   - For Summary, use [[Quantiles]]
+  *   - For Histogram, use [[Buckets]]
+  *
+  * See https://prometheus.io/docs/practices/histograms/
+  */
+sealed trait SamplingConfig
 
 final case class Quantiles(qs: List[Quantile], maxAge: FiniteDuration = 10.minutes, ageBuckets: Int = 5)
-    extends HistogramConfig
-    with TimerConfig
+    extends SamplingConfig
 
 object Quantiles {
 
@@ -46,14 +49,54 @@ object Quantiles {
   }
 }
 
-sealed trait Buckets extends HistogramConfig with TimerConfig
+sealed trait Buckets extends SamplingConfig
 
+/** ClassicBuckets defines the bucket upper bounds for the classic histogram buckets
+  * @param bs
+  *   upper bounds
+  */
 final case class ClassicBuckets(bs: List[Double]) extends Buckets
 object ClassicBuckets {
   def apply(b: Double*): ClassicBuckets = ClassicBuckets(b.toList)
 }
+
+/** ClassicLinearBuckets defines the bucket upper bounds with linear boundaries
+  *
+  * @param start
+  *   the first bucket boundary
+  * @param width
+  *   the width of each bucket
+  * @param count
+  *   the total number of buckets, including start
+  */
 final case class ClassicLinearBuckets(start: Double, width: Double, count: Int) extends Buckets
+
+/** ClassicExponentialBuckets defines the bucket upper bounds with exponential boundaries
+  *
+  * @param start
+  *   the first bucket boundary
+  * @param factor
+  *   the growth factor
+  * @param count
+  *   the total number of buckets, including start
+  */
 final case class ClassicExponentialBuckets(start: Double, factor: Double, count: Int) extends Buckets
+
+/** NativeBuckets defines the behavior for the native histogram buckets.
+  *
+  * See https://prometheus.io/docs/specs/native_histograms/
+  *
+  * @param initialSchema
+  *   the resolution of the native histogram
+  * @param minZeroThreshold
+  *   the initial and minimum value of the zero threshold
+  * @param maxZeroThreshold
+  *   the maximum value of the zero threshold
+  * @param maxNumberOfBuckets
+  *   the maximum number of buckets
+  * @param resetDuration
+  *   the delay after histogram may seset when native histogram buckets exceeds nativeMaxBuckets (0 indicates no reset)
+  */
 final case class NativeBuckets(
     initialSchema: Int = 5,
     minZeroThreshold: Double = Math.pow(2.0, -128),
@@ -78,6 +121,31 @@ object PrometheusMetricsNames {
   )
 }
 
+/** Prometheus metrics settings
+  *
+  * @param namespace
+  *   Metrics namespace
+  * @param metricsNames
+  *   Name of the individual metrics
+  * @param defineError
+  *   Function that defines if the http response should be counted as an error
+  * @param includeMethodDimension
+  *   Include the method dimension on metrics
+  * @param includePathDimension
+  *   Include the path dimension on metrics
+  * @param includeStatusDimension
+  *   Include the status group dimension on metrics
+  * @param serverDimensions
+  *   Static dimensions to be set on all metrics
+  * @param customDimensions
+  *   Custom dimensions
+  * @param receivedBytesConfig
+  *   Sampling configuraton to recrod recieved bytes
+  * @param durationConfig
+  *   Sampling configuraton to recrod server response duration
+  * @param sentBytesConfig
+  *   Sampling configuraton to recrod sent bytes
+  */
 final case class PrometheusSettings(
     namespace: String,
     metricsNames: HttpMetricsNames,
@@ -87,9 +155,9 @@ final case class PrometheusSettings(
     includeStatusDimension: Boolean,
     serverDimensions: immutable.Seq[Dimension] = immutable.Seq.empty,
     customDimensions: immutable.Seq[HttpMessageLabeler] = immutable.Seq.empty,
-    receivedBytesConfig: HistogramConfig,
-    durationConfig: TimerConfig,
-    sentBytesConfig: HistogramConfig
+    receivedBytesConfig: SamplingConfig,
+    durationConfig: SamplingConfig,
+    sentBytesConfig: SamplingConfig
 ) extends HttpMetricsSettings {
 
   def withNamespace(namespace: String): PrometheusSettings                 = copy(namespace = namespace)
@@ -100,9 +168,9 @@ final case class PrometheusSettings(
   def withIncludeStatusDimension(include: Boolean): PrometheusSettings     = copy(includeStatusDimension = include)
   def withServerDimensions(dims: Dimension*): PrometheusSettings           = copy(serverDimensions = dims.toVector)
   def withCustomDimensions(dims: HttpMessageLabeler*): PrometheusSettings  = copy(customDimensions = dims.toVector)
-  def withReceivedBytesConfig(config: HistogramConfig): PrometheusSettings = copy(receivedBytesConfig = config)
-  def withDurationConfig(config: TimerConfig): PrometheusSettings          = copy(durationConfig = config)
-  def withSentBytesConfig(config: HistogramConfig): PrometheusSettings     = copy(sentBytesConfig = config)
+  def withReceivedBytesConfig(config: SamplingConfig): PrometheusSettings  = copy(receivedBytesConfig = config)
+  def withDurationConfig(config: SamplingConfig): PrometheusSettings       = copy(durationConfig = config)
+  def withSentBytesConfig(config: SamplingConfig): PrometheusSettings      = copy(sentBytesConfig = config)
 }
 
 object PrometheusSettings {
